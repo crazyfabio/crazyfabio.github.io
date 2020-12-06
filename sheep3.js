b1fbb6d2b103074acc55fc7590cc45e7208174ee
Fabio <empiresmt2@gmail.com>
2020-12-06 22:56:54 +0100

    Update sheep3.js

--- sheep3.js
+++ sheep3.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(e) {
   var homeButton = document.createElement("sheep-btn");
   homeButton.setAttribute('tabindex', 0);
-  homeButton.setAttribute('aria-label', 'Go to SheepTester directory');
+  homeButton.setAttribute('aria-label', 'Clicca qui se hai bisogno di aiuto');
   homeButton.addEventListener('click', function(e) {
     document.body.classList.add('sheep-blockscreen');
     homeButton.addEventListener('transitionend', function(e) {
       setTimeout(function() {
-        window.location = '/?from=sheep3';
+        window.location = 'www.ezhub.it';
       }, 100);
     });
   });
